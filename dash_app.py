91873cfeec057bb96e11ad18890773d417b06719
Tshepiso <tlhongtshepiso2@gmail.com>
2025-06-29 15:32:42 +0200

    update

--- dash_app.py
+++ dash_app.py
@@ -1,6 +1,7 @@
 from  dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.express as px # datavisualization library
 import pandas as pd     # data manipulation library
+import csv
 '''
 dcc : Dash Core Components
 html: HTML components
@@ -8,14 +9,32 @@ html: HTML components
 
 dataframe = pd.read_csv('history_pink_morsel.csv')
 
+def filter_region(file_name):
+    with open('combined.csv', mode='r') as file:
+        csv_reader = csv.reader(file)
+        
+        with open(file_name + '.csv', mode="w") as east_file:
+            east_writer = csv.writer(east_file)
+
+            for line in csv_reader:
+                if file_name in line:
+                    east_writer.writerow(line)
+
+filter_region('north')
+filter_region('south')
+filter_region('east')
+filter_region('west')
+
+
 app = Dash()
 
 app.layout = [  # components are displayed in here, they are provided as a list
     html.H1("Pink Morsek Sales Histor", style={'textAlign': 'center'}), # Heading of graph
     html.Hr(), # horizontal line
-    dcc.RadioItems(options=['sale','date','region'], value='sale', id='controls-and-radio-item'),
     dash_table.DataTable(data=dataframe.to_dict('records'), page_size=10),
     html.Hr(),
+    dcc.RadioItems(options=['sale','east'], value='sale', id='controls-and-radio-item', style={'textAlign': 'center'}),
+
     #dcc.Graph(figure=px.line(dataframe, x='date', y='sale', title='Pink Morsel Sales Over Time')), 
     dcc.Graph(figure={}, id='controls-and-graph')
 ]
--- main.py
+++ main.py
@@ -1,2 +1,49 @@
-#!/bin/python3
-https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv
+import csv
+
+
+def open_file(file):
+
+    with open(file, mode="r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        with open("combined.csv", mode="a") as combined_files:
+
+            csv_writer = csv.writer(combined_files)
+            
+            for line in csv_reader:
+                if line[0] == "pink morsel":
+                    csv_writer.writerow(line)
+    
+             
+
+#open_file("data/daily_sales_data_0.csv")
+
+def combine_files(*args):
+
+    for file in args:
+        open_file(file)
+
+
+#combine_files("data/daily_sales_data_0.csv","data/daily_sales_data_0.csv","data/daily_sales_data_0.csv")
+
+def data_processing(*args):    
+    for file in args:
+        with open(file, mode="r") as csv_file:
+
+            csv_reader = csv.reader(csv_file)
+
+            with open("combined.csv", mode="a") as combined_files:
+
+                csv_writer = csv.writer(combined_files)
+                
+                for line in csv_reader:
+                    if line[0] == "pink morsel":
+                        if "$" in line[1]:
+                            line[1] = line[1].replace("$", "")
+                            sales = f"${float(line[1]) * int(line[2]):.2f}"
+                            row = sales, line[3], line[4]
+                            csv_writer.writerow(row)
+
+                 
+
+data_processing("data/daily_sales_data_0.csv","data/daily_sales_data_1.csv","data/daily_sales_data_2.csv")
